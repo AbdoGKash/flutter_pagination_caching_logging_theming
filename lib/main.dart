04cbbc4cf3108d8512ddb5d4d8934d7c9e22fd10
Abdo Gamal Kashkoush <160290297+AbdoGKash@users.noreply.github.com>
2025-10-31 18:59:54 +0200

    integrate Firebase Crashlytics for error logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pagination_caching_logging_theming/core/helper/injection.dart';
+import 'package:flutter_pagination_caching_logging_theming/firebase_options.dart';
 import 'package:flutter_pagination_caching_logging_theming/presentation/cubit/theme/theme_cubit.dart';
 import 'package:flutter_pagination_caching_logging_theming/presentation/cubit/theme/theme_state.dart';
 import 'package:flutter_pagination_caching_logging_theming/presentation/cubit/movies_cubit.dart';
 import 'package:flutter_pagination_caching_logging_theming/presentation/home_screen.dart';
 
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await initGetIt();
+//   runApp(const MoviesApp());
+// }
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await initGetIt();
-  runApp(const MoviesApp());
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (kDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      FirebaseCrashlytics.instance.recordFlutterError(details);
+    }
+  };
+  runZonedGuarded<Future<void>>(
+    () async {
+      runApp(const MoviesApp());
+    },
+    (Object error, StackTrace stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    },
+  );
 }
 
 class MoviesApp extends StatelessWidget {
